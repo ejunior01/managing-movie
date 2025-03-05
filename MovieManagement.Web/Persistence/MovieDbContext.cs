3629a973eb1fc9b500e57e257e2e7c87446fb351
Edvan Ribeiro Dos Santos Junior <jk.jr47@gmail.com>
2025-03-05 01:02:15 +0000

    Add movie class configuration and first migration

--- MovieManagement.Web/Persistence/MovieDbContext.cs
+++ MovieManagement.Web/Persistence/MovieDbContext.cs
@@ -14,4 +14,41 @@ public class MovieDbContext(DbContextOptions<MovieDbContext> options) : DbContex
         builder.ApplyConfigurationsFromAssembly(typeof(MovieDbContext).Assembly);
         base.OnModelCreating(builder);
     }
+
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder
+             .UseAsyncSeeding(async (context, _, cancellationToken) =>
+             {
+                 var sampleMovie = await context.Set<Movie>().FirstOrDefaultAsync(
+                    b => b.Title == "Sonic the Hedgehog 3",
+                    cancellationToken: cancellationToken);
+
+                 if (sampleMovie == null)
+                 {
+                     sampleMovie = Movie.Create("Sonic the Hedgehog 3",
+                                                "Fantasy",
+                                                new DateTimeOffset(new DateTime(2025, 1, 3), TimeSpan.Zero),
+                                                7);
+
+                     await context.Set<Movie>().AddAsync(sampleMovie, cancellationToken);
+                     await context.SaveChangesAsync(cancellationToken);
+                 }
+             })
+             .UseSeeding((context, _) =>
+             {
+                 var sampleMovie = context.Set<Movie>().FirstOrDefault(b => b.Title == "Sonic the Hedgehog 3");
+                 if (sampleMovie == null)
+                 {
+                     sampleMovie = Movie.Create("Sonic the Hedgehog 3",
+                                                "Fantasy",
+                                                new DateTimeOffset(new DateTime(2025, 1, 3), TimeSpan.Zero),
+                                                7);
+                                                
+                     context.Set<Movie>().Add(sampleMovie);
+                     context.SaveChanges();
+                 }
+             });
+    }
 }
--- MovieManagement.Web/Program.cs
+++ MovieManagement.Web/Program.cs
@@ -1,15 +1,33 @@
+using Microsoft.EntityFrameworkCore;
+using MovieManagement.Web.Persistence;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddOpenApi();
 
+builder.Services.AddDbContext<MovieDbContext>(options =>
+{
+    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+    options.UseNpgsql(connectionString);
+});
+
+
 var app = builder.Build();
 
-if(app.Environment.IsDevelopment()) {
+
+// await using (var serviceScope = app.Services.CreateAsyncScope())
+// await using (var dbContext = serviceScope.ServiceProvider.GetRequiredService<MovieDbContext>())
+// {
+//     await dbContext.Database.EnsureCreatedAsync();
+// }
+
+
+if (app.Environment.IsDevelopment())
+{
     app.MapOpenApi();
     app.MapScalarApiReference();
 }
 
-app.MapGet("/", () => "Hello World!").Produces(200,typeof(string));
+app.MapGet("/", () => "Hello World!").Produces(200, typeof(string));
 
 app.Run();
